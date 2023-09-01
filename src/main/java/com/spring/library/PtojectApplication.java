00fb5273015c2f611b7cee7c4506b165fb424d8f
InnaKorniychuk <innakorniychuk034@gmail.com>
2023-09-01 15:29:05 +0300

    added pre-alpha

---
+++ src/main/java/com/spring/library/PtojectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/library/PtojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
